87b1f813a1b08cefa285cf033ccd28abe56d4416
JunYeong Park <qkrwns0727@gmail.com>
2023-02-16 17:00:41 +0900

    Establish an authentication system using Decorator and create an administrative super account and media

--- oiccml/settings.py
+++ oiccml/settings.py
@@ -143,3 +143,8 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 # logout 은 바로 /login 경로로 가도록 REDIRECT
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
