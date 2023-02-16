81d32f4547858f2251446fb134fd65932faa43cd
JunYeong Park <qkrwns0727@gmail.com>
2023-02-16 23:33:45 +0900

    Refactoring the code while making profileapp

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -15,7 +15,9 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
+    # success_url 을 accountapp:detail 로 보내 주고 싶지만 pk 값을 따로 넘겨줄 수가 없다.
+    # 그래서 아래 get_success_url 함수로 덮어씌워서 새로운 걸 만들어줘야 한다.
     template_name = 'profileapp/create.html'
 
     # form 변수에 우리가 입력한 fields 값들이 들어 있다(fields 는 forms.py에 정의 되어 있음)
@@ -26,6 +28,10 @@ class ProfileCreateView(CreateView):
         temp_profile.save()  # 여기서 최종적으로 해당 프로필 정보를 save 저장 한다
         return super().form_valid(form)
 
+    def get_success_url(self):
+        # self 는 Profile 이라는 model 이기 때문에 이 오브젝트의 user에 있는 pk 값을 넘겨준다.
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -33,5 +39,9 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        # self 는 Profile 이라는 model 이기 때문에 이 오브젝트의 user에 있는 pk 값을 넘겨준다.
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
